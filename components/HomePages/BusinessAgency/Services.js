3d8087ef8d8b7e25046e66100ca995aeba1e7f43
vishnusudev <vishnusudev2001@gmail.com>
2023-07-31 22:29:29 +0530

    card

--- components/HomePages/BusinessAgency/Services.js
+++ components/HomePages/BusinessAgency/Services.js
@@ -6,56 +6,65 @@ import "reactjs-popup/dist/index.css";
 const servicesData = [
   {
     iconName: "pe-7s-rocket bg-13c4a1",
-    title: "SEO Optimization",
+    title: "Digital Advertising Campaigns",
     shortText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+      "Developing and managing data-driven digital advertising campaigns across various platforms, such as Google Ads, Facebook, and other display networks, to drive targeted traffic and conversions.",
     viewDetails: "/service-details",
     aosDelay: "100",
   },
   {
     iconName: "pe-7s-diamond bg-6610f2",
-    title: "Market Research",
+    title: "Conversion Rate Optimization",
     shortText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+      "Analyzing user behavior, optimizing landing pages, and implementing A/B testing to increase the percentage of website visitors who take desired actions, resulting in improved ROI",
     viewDetails: "/service-details",
     aosDelay: "200",
   },
   {
     iconName: "pe-7s-light bg-ffb700",
-    title: "Digital Marketing",
+    title: "Search Engine Optimization",
     shortText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
-    viewDetails: "/service-details",
+      "Implementing strategic SEO techniques to improve organic search rankings, increase website visibility, and drive qualified traffic, ultimately boosting ROI over time",
+      viewDetails: "/service-details",
     aosDelay: "300",
   },
   {
     iconName: "pe-7s-target bg-fc3549",
-    title: "Keyword Targeting",
+    title: "Data Analytics and Report",
     shortText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+      "Utilizing advanced analytics tools to track and measure campaign performance, user engagement, and conversions, providing clients with actionable insights to optimize ROI-driven strategies.",
     viewDetails: "/service-details",
     aosDelay: "400",
   },
   {
     iconName: "pe-7s-mail-open-file bg-00d280",
-    title: "Email Marketing",
+    title: "Performance Monitoring",
     shortText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+      "Constantly monitoring campaign performance, adjusting strategies, and optimizing ad placements, keywords, and targeting to achieve the best possible ROI for clients.",
     viewDetails: "/service-details",
     aosDelay: "500",
   },
   {
     iconName: "pe-7s-users bg-ff612f",
-    title: "Marketing & Reporting",
+    title: "Marketing CRM development",
     shortText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+      "User-centric and visually stunning apps, optimized for seamless user experiences. Leveraging data-driven insights, we create apps that engage users, drive conversions, and boost your brand's ROI",
     viewDetails: "/service-details",
     aosDelay: "600",
+  },
+  {
+    iconName: "pe-7s-users bg-ff612f",
+    title: "Omni-channel      retail",
+    shortText:
+      "Omnichannel retail strategies that empower your brand to thrive in today's competitive market. By synchronizing messaging and promotions across platforms like social media, email, website, and mobile apps, we ensure a consistent and captivating customer experience.",
+    viewDetails: "/service-details",
+    aosDelay: "700",
   },
 ];
 
 const Services = () => {
   const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState("");
 
   return (
     <>
@@ -71,9 +80,9 @@ const Services = () => {
 
           <div className="row justify-content-center">
             {servicesData &&
-              servicesData.slice(0, 6).map((value, i) => (
+              servicesData.slice(0, 7).map((value, i) => (
                 <div
-                  className="col-lg-4 col-sm-6"
+                  className="col-lg-3 col-sm-6"
                   key={i}
                   data-aos="fade-in"
                   data-aos-duration="1200"
@@ -96,6 +105,7 @@ const Services = () => {
                     <button
                       onClick={() => {
                         setShowModal(true);
+                        setData(value.shortText)
                       }}
                       className="btn"
                     >
@@ -110,7 +120,7 @@ const Services = () => {
             setShowModal(false);
           }}
           position="right center">
-            <div>Popup content here !!</div>
+            <div>{data}</div>
           </Popup>
         </div>
       </div>
