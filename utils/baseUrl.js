b93b6d80ec02431dcc4301f769aaaa71f2a9b349
devsanjithm <dev.sanjithm@gmail.com>
2023-08-11 13:13:45 +0530

    message-send-mobile-working

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://trynext-react.envytheme.com' 
+? 'https://firstscrolldemo2.vercel.app/' 
 : 'http://localhost:3000';
 
 export default baseUrl;
